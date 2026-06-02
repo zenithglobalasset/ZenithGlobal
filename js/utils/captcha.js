43d8cc19261c55aa4afb461bc17c38e6e728470a
zenithglobalasset <zenithglobalassets@gmail.com>
2026-06-02 23:17:44 +0300

    Delete js directory

--- js/utils/captcha.js
+++
@@ -1,6 +0,0 @@
-export function generateCaptcha() {
-    const n1 = Math.floor(Math.random() * 10) + 1;
-    const n2 = Math.floor(Math.random() * 10) + 1;
-    document.getElementById('captchaText').innerText = `${n1} + ${n2} = ?`;
-    return n1 + n2;
-}
