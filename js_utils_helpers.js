baaaf4af8d0d2ea47a65dbb33282f19c35c0e168
zenithglobalasset <zenithglobalassets@gmail.com>
2026-06-03 21:18:45 +0300

    Update js_utils_helpers.js

--- js_utils_helpers.js
+++ js_utils_helpers.js
@@ -1,34 +1,15 @@
-import { auth, db } from '../config.js';
-import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
-import { generateCaptcha, loadCountries } from '../utils/helpers.js';
+export function generateCaptcha() {
+    return Math.random().toString(36).substring(7).toUpperCase();
+}
 
-const countrySelect = document.getElementById('country');
-const captchaDiv = document.getElementById('captcha-container');
-let currentCaptcha = generateCaptcha();
-
-loadCountries(countrySelect);
-captchaDiv.textContent = currentCaptcha;
-
-document.getElementById('registerForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    if (document.getElementById('captchaInput').value !== currentCaptcha) {
-        alert("Invalid Captcha!");
-        currentCaptcha = generateCaptcha();
-        captchaDiv.textContent = currentCaptcha;
-        return;
-    }
-    try {
-        const userCredential = await createUserWithEmailAndPassword(auth, document.getElementById('email').value, document.getElementById('password').value);
-        await setDoc(doc(db, "users", userCredential.user.uid), {
-            firstName: document.getElementById('firstName').value,
-            lastName: document.getElementById('lastName').value,
-            country: countrySelect.value,
-            email: userCredential.user.email,
-            createdAt: new Date()
-        });
-        await sendEmailVerification(userCredential.user);
-        alert("Registration Success! Please verify your email.");
-        window.location.href = "login.html";
-    } catch (err) { alert(err.message); }
-});
+export async function loadCountries(selectElement) {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data = await response.json();
+    data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+    data.forEach(c => {
+        let opt = document.createElement("option");
+        opt.value = c.name.common;
+        opt.textContent = c.name.common;
+        selectElement.appendChild(opt);
+    });
+}
